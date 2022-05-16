8d9e8b70019a630c05551252d8eb23692b45616a
Yasuhisa Tanioka <tanioka@xform.xyz>
2022-05-16 16:47:53 +0900

    Delete unnecessary files for cmake-mode.el

--- Tests/RunCMake/VS10Project/VsNoCompileBatching.cmake
+++
@@ -1,9 +0,0 @@
-enable_language(CXX)
-
-add_library(foo foo.cpp)
-
-add_library(foo_NB foo.cpp)
-set_property(TARGET foo_NB PROPERTY VS_NO_COMPILE_BATCHING ON)
-
-add_library(foo_NB_OFF foo.cpp)
-set_property(TARGET foo_NB_OFF PROPERTY VS_NO_COMPILE_BATCHING OFF)
--- Tests/RunCMake/VerifyHeaderSets/a.h
+++
@@ -1,5 +0,0 @@
-#ifdef TEST_A_H
-#  error "TEST_A_H defined"
-#endif
-
-extern void a_h(void);
